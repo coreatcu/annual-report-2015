67b85d0d651520cc9bccd1c476c25455c6099b0d
Jackie Luo <jacquelineluo95@gmail.com>
2015-12-20 02:16:46 -0500

    Include temporary Impulse photo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $("a[href^='#']").on('click', function(e) {
    var hash = this.hash;
    $('html, body').animate({
        scrollTop: $(hash).offset().top
-     }, 300, function(){
+     }, 800, function(){
        window.location.hash = hash;
      });
 });
